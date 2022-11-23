70aad384ec1a782c5e11ec8544410918fd68a174
Arno Richter <mail@arnorichter.de>
2022-11-23 15:35:20 +0100

    Update and rename site/scripts/build.php to build.php

--- build.php
+++ build.php
@@ -1,11 +1,12 @@
 <?php
 
 define('DS', DIRECTORY_SEPARATOR);
-require('..' . DS . 'kirby' . DS . 'bootstrap.php');
+require('.' . DS . 'kirby' . DS . 'bootstrap.php');
 
 $kirby = kirby();
 $site = $kirby->site();
 
+var_dump($kirby);
+
 // $staticSiteGenerator = new D4L\StaticSiteGenerator($kirby, $pathsToCopy = null, $pages = null);
 // $fileList = $staticSiteGenerator->generate($outputFolder = './static', $baseUrl = '/', $preserve = []);
-
