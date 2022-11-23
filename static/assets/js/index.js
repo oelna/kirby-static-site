33bb91bc5b663971529785fd7c281686399da14e
Arno Richter <mail@arnorichter.de>
2022-11-23 16:04:46 +0100

    Delete static directory

--- static/assets/js/index.js
+++
@@ -1,7 +0,0 @@
-// Lightbox
-Array.from(document.querySelectorAll("[data-lightbox]")).forEach(element => {
-  element.onclick = (e) => {
-    e.preventDefault();
-    basicLightbox.create(`<img src="${element.href}">`).show();
-  };
-});
